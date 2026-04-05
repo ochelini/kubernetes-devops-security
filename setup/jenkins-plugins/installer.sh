cb962f3024a2e89e0696e7858ce6b12d71ead312
ochelini <interac@baelio.com>
2026-04-05 17:11:58 -0300

    Refactor Jenkins plugin installer script
    Updated the installer script for Jenkins plugins to improve the process of obtaining the Jenkins crumb and API token, and modified the plugin installation method.

--- setup/jenkins-plugins/installer.sh
+++ setup/jenkins-plugins/installer.sh
@@ -1,33 +1,48 @@
 #!/bin/bash
-
 set -eo pipefail
 
-JENKINS_URL='http://localhost:8080'
+JENKINS_URL="http://localhost:8080"
 
-JENKINS_CRUMB=$(curl -s --cookie-jar /tmp/cookies -u admin:admin ${JENKINS_URL}/crumbIssuer/api/json | jq .crumb -r)
+echo "=== Waiting for Jenkins to start ==="
+until curl -s $JENKINS_URL/login > /dev/null; do
+    echo "Jenkins not ready yet..."
+    sleep 5
+done
 
-JENKINS_TOKEN=$(curl -s -X POST -H "Jenkins-Crumb:${JENKINS_CRUMB}" --cookie /tmp/cookies "${JENKINS_URL}/me/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken?newTokenName=demo-token66" -u admin:admin | jq .data.tokenValue -r)
+echo "=== Reading initial admin password ==="
+ADMIN_PASS=$(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
 
-echo $JENKINS_URL
-echo $JENKINS_CRUMB
-echo $JENKINS_TOKEN
+echo "=== Getting Jenkins crumb ==="
+CRUMB=$(curl -s -u "admin:${ADMIN_PASS}" "$JENKINS_URL/crumbIssuer/api/json" | jq -r .crumb)
 
+echo "=== Creating API token ==="
+TOKEN=$(curl -s -X POST \
+    -u "admin:${ADMIN_PASS}" \
+    -H "Jenkins-Crumb:${CRUMB}" \
+    -H "Content-Type: application/json" \
+    -d '{"newTokenName": "automation-token"}' \
+    "$JENKINS_URL/user/admin/descriptorByName/jenkins.security.ApiTokenProperty/generateNewToken" \
+    | jq -r .data.tokenValue)
+
+echo "Jenkins URL: $JENKINS_URL"
+echo "Crumb: $CRUMB"
+echo "API Token: $TOKEN"
+
+echo "=== Installing plugins ==="
 while read plugin; do
-   echo "........Installing ${plugin} .."
-   curl -s POST --data "<jenkins><install plugin='${plugin}' /></jenkins>" -H 'Content-Type: text/xml' "$JENKINS_URL/pluginManager/installNecessaryPlugins" --user "admin:$JENKINS_TOKEN"
+    echo "Installing plugin: $plugin"
+    curl -s -X POST \
+        -u "admin:${TOKEN}" \
+        -H "Jenkins-Crumb:${CRUMB}" \
+        -H "Content-Type: text/xml" \
+        --data "<jenkins><install plugin='${plugin}' /></jenkins>" \
+        "$JENKINS_URL/pluginManager/installNecessaryPlugins"
 done < plugins.txt
 
+echo "=== Triggering Jenkins safe restart ==="
+curl -s -X POST \
+    -u "admin:${TOKEN}" \
+    -H "Jenkins-Crumb:${CRUMB}" \
+    "$JENKINS_URL/safeRestart"
 
-#### we also need to do a restart for some plugins
-
-#### check all plugins installed in jenkins
-# 
-# http://<jenkins-url>/script
-
-# Jenkins.instance.pluginManager.plugins.each{
-#   plugin -> 
-#     println ("${plugin.getDisplayName()} (${plugin.getShortName()}): ${plugin.getVersion()}")
-# }
-
-
-#### Check for updates/errors - http://<jenkins-url>/updateCenter
+echo "=== Jenkins plugin installation complete ==="
